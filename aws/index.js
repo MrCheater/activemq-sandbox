1637789607e27a88334847705b9e1025a444ec55
Max Vasin <maksim.vasin@devexpress.com>
2018-03-30 18:09:11 +0300

    aws working

--- aws/index.js
+++ aws/index.js
@@ -1,38 +1,44 @@
 const { connect } = require('stompit');
 
 const connectOpts = {
-  'host': 'b-83ef68a6-7d58-4401-a156-edad4b8cd5fe-1.mq.eu-central-1.amazonaws.com',
+  'host': 'b-8cea65a2-a6e1-4ec8-85f8-86d0e4c93c6a-1.mq.eu-central-1.amazonaws.com',
   'port': 61614,
   'ssl': true,
   'connectHeaders': {
     'host': '/',
-    'login': 'admin',
-    'passcode': 'admin',
+    'login': 'client',
+    'passcode': 'gwJKORpKYI1U',
     'heart-beat': '5000,5000'
   }
 };
 
 const topic = '/topic/com.resolvejs.sample';
 
-connect(connectOpts, (err, client) => {
-  if (err) {
-    console.log('Oh no :( - ' + err.message);
-    return;
-  }
+exports.handler = (event, context, callback) => {
+  connect(connectOpts, (err, client) => {
+    if (err) {
+      console.error('Oh no :( - ' + err.message);
+      return;
+    }
 
-  let frame = client.send({
-    'destination': topic,
-    'content-type': 'application/json',
-    'eventType': 'a'
-  });
-  frame.write(JSON.stringify({ type: 'a-type', timestamp: Date.now(), payload: { name: 'Sarah Connor' } }));
-  frame.end();
+    let frame = client.send({
+      'destination': topic,
+      'content-type': 'application/json',
+      'eventType': 'a'
+    });
+    frame.write(JSON.stringify({ type: 'a-type', timestamp: Date.now(), payload: { name: 'Sarah Connor' } }));
+    frame.end();
 
-  frame = client.send({
-    'destination': topic,
-    'content-type': 'application/json',
-    'eventType': 'b'
+    frame = client.send({
+      'destination': topic,
+      'content-type': 'application/json',
+      'eventType': 'b'
+    });
+    frame.write(JSON.stringify({ type: 'b-type', timestamp: Date.now(), payload: { name: 'John Smith' } }));
+    frame.end();
+
+    client.disconnect();
+
+    callback(null, 'Done');
   });
-  frame.write(JSON.stringify({ type: 'b-type', timestamp: Date.now(), payload: { name: 'John Smith' } }));
-  frame.end();
-});
+};
